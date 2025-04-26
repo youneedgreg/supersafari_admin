fd9b65d6a47e43ed2b3903d4d7ce9c68b866ab26
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-04-26 20:28:16 +0300

    sonner-provider.tsx

---
+++ src/components/sonner-provider.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Toaster as SonnerToaster } from "sonner"
+
+export function SonnerProvider() {
+  return (
+    <SonnerToaster
+      position="top-right"
+      toastOptions={{
+        style: {
+          background: "white",
+          color: "black",
+          border: "1px solid #E2E8F0",
+        },
+        className: "border border-border shadow-lg",
+      }}
+      theme="light"
+      closeButton
+    />
+  )
+}
