096c16ef3033d21a28bcd48f788b68b0aca1ebee
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-04-27 15:14:37 +0300

    database configuration file

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import mysql, { Connection } from 'mysql2/promise';
+
+export async function connectDB(): Promise<Connection> {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST!,
+user: process.env.DB_USER!,
+password: process.env.DB_PASSWORD!,
+database: process.env.DB_DATABASE!,
+  });
+
+  return connection;
+}
