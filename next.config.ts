03cb66f7b94f444426cef9faefcddbc47e8ca4b0
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-05-18 01:08:25 +0300

    env on next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
 };
 
 export default nextConfig;
